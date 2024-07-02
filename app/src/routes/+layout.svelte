e2bb9f727723f46be8028f5feea87bd2b2a3d37c
Punyaslok <12557052+bitsnorbytes@users.noreply.github.com>
2024-07-02 19:32:02 +0530

    Changes to todo updates

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -1,5 +1,8 @@
 <script>
     import "../app.css";
-  </script>
-  
-  <slot />
+</script>
+<header class="flex flex-col items-center">
+  <span class="text-2xl font-bold">docket</span>
+  <span class="text-xs uppercase font-bold tracking-[0.4em]">Your Todo App</span>
+</header>
+<slot />
